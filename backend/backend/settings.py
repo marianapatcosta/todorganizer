ce17cce6c230bfd396dc9f53cf2aa4d1d8940a30
Mariana Costa <43031902+marianapatcosta@users.noreply.github.com>
2021-02-26 21:27:47 +0000

    defined allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,9 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',          
     'rest_framework', 
     'rest_framework.authtoken',
+    'corsheaders',          
     'todo'
 ]
 
