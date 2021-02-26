660bb0ad34938a41815df8ee228db58eb99a9f55
Mariana Costa <43031902+marianapatcosta@users.noreply.github.com>
2021-02-26 20:46:01 +0000

    defined allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,7 @@ DEBUG = env('DEBUG') != 'False'
 
 SECRET_KEY = env('SECRET_KEY')
 
-ALLOWED_HOSTS = [ 'http://localhost:3000', 'https://todorganizer.web.app'] 
+ALLOWED_HOSTS = [ '*'] 
 
 
 # Application definition
@@ -136,10 +136,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # allow origin localhost:3000
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = (
+CORS_ORIGIN_ALLOW_ALL = True
+""" CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'https://todorganizer.web.app'
-)
+) """
 
 django_heroku.settings(locals())
